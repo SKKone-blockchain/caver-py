a7996edc8cbf2708027087f18968f6927309310e
Seungjin Lee <lsg0385@naver.com>
2020-03-14 23:17:24 +0900

    [ADD] getBlockTest 코드 추가

--- test.py
+++ test.py
@@ -1,6 +1,25 @@
 from caver.caver import Caver
 
+caver = Caver("https://api.baobab.klaytn.net:8651")
 def provider_test():
   caver = Caver("https://api.baobab.klaytn.net:8651")
   block = caver.klay.blocks.getBlockByNumber()
 provider_test()
+
+def getBlockTest():
+  blockNum = caver.klay.blocks.getCurrentBlockNumber()
+  print('current block number:', blockNum)
+  '''
+  getBlockByNumber와 getBlockByHash는 서로 다른 데이터를 가져오므로 사용에 따라 유의
+  '''
+  block = caver.klay.blocks.getBlockByNumber(blockNum)
+  print('current block:', block)
+  nblock = caver.klay.blocks.getBlockByHash(block.hash)
+  print('get block by hash({})\n'.format(block.hash), nblock)
+
+  if block.transactions_root == nblock.transactions_root:
+    print('동일한 블록이 맞습니다.')
+  else:
+    print('다른 블록입니다.')
+
+getBlockByHash()
