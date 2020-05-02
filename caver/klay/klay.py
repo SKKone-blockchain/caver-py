fb6f7efba134b89b48e8a8195799b6c473653b95
pleiades-s <rudobco@g.skku.edu>
2020-05-02 14:20:55 +0900

    Block function from caver dir

--- caver/klay/klay.py
+++ caver/klay/klay.py
@@ -14,24 +14,3 @@ class Klay:
 
     else:
       return self.blocks.getBlockWithConsensusInfoByNumber(hex(inputs))
-
-  def getCommittee(self, defaultBlock="latest"):
-    return self.blocks.getCommittee(defaultBlock)
-
-  def getCommitteeSize(self, defaultBlock="latest"):
-    return self.blocks.getCommitteeSize(defaultBlock)
-
-  def getCouncil(self, defaultBlock="latest"):
-    return self.blocks.getCouncil(defaultBlock)
-
-  def getCouncilSize(self, defaultBlock="latest"):
-    return self.blocks.getCouncilSize(defaultBlock)
-
-  def getStorageAt(self, address, position, defaultBlock="latest"):
-    return self.blocks.getStorageAt(address, position, defaultBlock)
-
-  def isMining(self):
-    return self.blocks.isMining()
-
-  def isSyncing(self):
-    return self.blocks.isSyncing()
