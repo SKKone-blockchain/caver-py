0258c4cf2ef40f8eea78109139d4a8cdc77d8482
pleiades-s <rudobco@g.skku.edu>
2020-05-02 14:21:49 +0900

    remove functions in klay

--- caver/klay/klay.py
+++ caver/klay/klay.py
@@ -7,10 +7,3 @@ class Klay:
     self.accounts = Accounts()
     self.blocks = Blocks(provider)
     self.defaultBlock = 'latest'
-
-  def getBlockWithConsensusInfo(self, inputs):
-    if type(inputs) == str:
-      return self.blocks.getBlockWithConsensusInfoByHash(inputs)
-
-    else:
-      return self.blocks.getBlockWithConsensusInfoByNumber(hex(inputs))
