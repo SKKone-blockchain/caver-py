f7e64047f51d971a72569953aabcbd6382ce39c6
pleiades-s <rudobco@g.skku.edu>
2020-05-04 22:03:07 +0900

    add __init__.py in accountUtil

--- test/klay/blocks/test_blocks.py
+++ test/klay/blocks/test_blocks.py
@@ -5,15 +5,15 @@ caver = Caver("https://api.baobab.klaytn.net:8651")
 def test_getBlockNumber_and_HashTest():
   blockNum = int(caver.klay.blocks.getCurrentBlockNumber(), 16)
   print('current block number:', blockNum)
-  '''
-  getBlockByNumber와 getBlockByHash는 서로 다른 데이터를 가져오므로 사용에 유의
-  '''
+  # '''
+  # NOTE: getBlockByNumber and getBlockByHash take different data
+  # '''
   block = caver.klay.blocks.getBlock(blockNum)
   print('current block:', block.number)
   nblock = caver.klay.blocks.getBlock(block.hash)
   print('get block by hash({})\n'.format(block.hash), nblock.number)
 
-  assert block.transactions_root == nblock.transactions_root, '다른 블록입니다.'
+  assert block.transactions_root == nblock.transactions_root, 'Different block'
 
 def test_getBlockReceipts():
   blockNum = caver.klay.blocks.getCurrentBlockNumber()
