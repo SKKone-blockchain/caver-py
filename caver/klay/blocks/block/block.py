3ccd9992e45254d43a22847617386eb061c7fc40
Seungjin Lee <lsg0385@naver.com>
2020-05-16 20:44:20 +0900

    [HOTFIX] Block class member blockscore => block_score

--- caver/klay/blocks/block/block.py
+++ caver/klay/blocks/block/block.py
@@ -10,7 +10,7 @@ class Block:
       except:
         return None
 
-    self.blockscore = getParam("blockscore")
+    self.block_score = getParam("blockscore")
     self.extra_data = getParam("extraData")
     self.gas_used = getParam("gasUsed")
     self.governance_data = getParam("governanceData")
